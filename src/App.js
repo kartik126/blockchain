1569cc2ecb80df7408167d505fe7e7627b931a0e
kartik126 <lavi9921@gmail.com>
2022-02-18 12:34:19 +0530

    navbar'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-function App() {
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
+
+import Navbar from "./Components/Navbar";
+
+import React, { Component } from "react";export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showMenu: true };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu = () => {
+    this.setState({ showMenu: !this.state.showMenu });
+  };
+  render() {
+    return (
+      <div className="App" > 
+        <Navbar showMenu={this.state.showMenu} toggleMenu={this.toggleMenu} />
+     
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -9,67 +9,88 @@ import TwitterIcon from "@mui/icons-material/Twitter";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 import AddToDriveIcon from "@mui/icons-material/AddToDrive";
-import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
+import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import Home from "./Home";
 export class Navbar extends Component {
   render() {
     return (
-      <div className="navbar-container">
-        <div className="hide-button">
-          <button>
-            <ArrowBackOutlinedIcon />
-            <p>hello</p>
-          </button>
-        </div>
-        <div className="logo-div">
-          <div class="w-12 h-12 bg-primary rounded-full"></div>
-          <div className="logo-name">
-            <h1>logo</h1>
+      <div className="main-div">
+        {this.props.showMenu ? (
+          <div className="navbar-container">
+            <div className="hide-button">
+              <button onClick={this.props.toggleMenu}>
+                <ArrowBackOutlinedIcon />
+              </button>
+            </div>
+            <div className="logo-div">
+              <div className="w-12 h-12 bg-primary rounded-full"></div>
+              <div className="logo-name">
+                <h1>logo</h1>
+              </div>
+            </div>
+            <div className="wallet-div">
+              <button className="wallet-button">
+                <AccountBalanceWalletOutlinedIcon />
+                Wallet
+              </button>
+            </div>
+            <div className="middle-buttons-div">
+              <button className="dvdx-button">
+                {" "}
+                <AddModeratorOutlinedIcon /> DVDX Price
+              </button>
+              <button className="signup-updates-button">
+                <AddCircleOutlineOutlinedIcon />
+                Sign Up Updates
+              </button>
+            </div>
+            <div className="user-media-div">
+              <div className="first-row">
+                <button>
+                  <LanguageOutlinedIcon />
+                </button>
+                <button>
+                  <TelegramIcon />
+                </button>
+                <button>
+                  <TwitterIcon />
+                </button>
+              </div>
+              <div className="second-row">
+                <button>
+                  <GitHubIcon />
+                </button>
+                <button>
+                  <DescriptionOutlinedIcon />
+                </button>
+                <button>
+                  <AddToDriveIcon />
+                </button>
+              </div>
+            </div>
+            <div className="bottom-buttons-div">
+              <button>Privacy Policy</button>
+              <button>Disclaimer</button>
+            </div>
           </div>
-        </div>
-        <div className="wallet-div">
-          <button className="wallet-button">
-            <AccountBalanceWalletOutlinedIcon />
-            Wallet
-          </button>
-        </div>
-        <div className="middle-buttons-div">
-          <button className="dvdx-button">
-            {" "}
-            <AddModeratorOutlinedIcon /> DVDX Price
-          </button>
-          <button className="signup-updates-button">
-            <AddCircleOutlineOutlinedIcon />
-            Sign Up Updates
-          </button>
-        </div>
-        <div className="user-media-div">
-          <div className="first-row">
-            <button>
-              <LanguageOutlinedIcon />
-            </button>
-            <button>
-              <TelegramIcon />
-            </button>
-            <button>
-              <TwitterIcon />
+          
+        ) : (
+          <div className="minimise-bar">
+            <div className="show-button">
+              <button onClick={this.props.toggleMenu}>
+                <ArrowForwardIcon />
+              </button>
+            </div>
+            <div className="min-logo-div">
+              <div className="w-12 h-12 bg-primary rounded-full"></div>
+            </div>
+            <button className="min-wallet-button">
+              <AccountBalanceWalletOutlinedIcon />
             </button>
           </div>
-          <div className="second-row">
-            <button>
-              <GitHubIcon />
-            </button>
-            <button>
-              <DescriptionOutlinedIcon />
-            </button>
-            <button>
-              <AddToDriveIcon />
-            </button>
-          </div>
-        </div>
-        <div className="bottom-buttons-div">
-          <button>Privacy Policy</button>
-          <button>Disclaimer</button>
-        </div>
+        )}
+         <Home/>
       </div>
     );
   }
